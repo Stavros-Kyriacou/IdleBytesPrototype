772361a56419ebddf8accc61f0549bba3355cace
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-12-23 11:53:04 +1100

    display number of components in text fields on inventory screen

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
@@ -12,6 +13,10 @@ public class Inventory : MonoBehaviour
     public int[,] ramInventory = new int[10, 10];
     public int[,] hddInventory = new int[10, 10];
     public int[] lootBoxes = new int[5];
+    public List<Text> levelTexts;
+    public List<Image> componentImages;
+    public List<Image> tierImages;
+    private int componentType = 1;
     private void Awake()
     {
         if (Instance == null)
@@ -31,30 +36,13 @@ public class Inventory : MonoBehaviour
             for (int j = 0; j < this.cpuInventory.GetLength(1); j++)
             {
                 this.cpuInventory[i, j] = Random.Range(1, 11);
-            }
-        }
-        for (int i = 0; i < this.gpuInventory.GetLength(0); i++)
-        {
-            for (int j = 0; j < this.gpuInventory.GetLength(1); j++)
-            {
                 this.gpuInventory[i, j] = Random.Range(1, 11);
-            }
-        }
-        for (int i = 0; i < this.ramInventory.GetLength(0); i++)
-        {
-            for (int j = 0; j < this.ramInventory.GetLength(1); j++)
-            {
                 this.ramInventory[i, j] = Random.Range(1, 11);
-            }
-        }
-        for (int i = 0; i < this.hddInventory.GetLength(0); i++)
-        {
-            for (int j = 0; j < this.hddInventory.GetLength(1); j++)
-            {
                 this.hddInventory[i, j] = Random.Range(1, 11);
             }
         }
-        // PrintInventory();
+        SelectComponent(1);
+        SelectTier(1);
     }
     public void PrintInventory()
     {
@@ -70,7 +58,57 @@ public class Inventory : MonoBehaviour
             Debug.Log(temp);
             temp = "";
         }
-
     }
+    public void SelectTier(int tier)
+    {
+        foreach (var img in tierImages)
+        {
+            img.color = Color.white;
+        }
 
+        tierImages[tier - 1].color = Color.green;
+
+        switch (componentType)
+        {
+            case 1:
+                //cpu
+                for (int i = 0; i < this.levelTexts.Count; i++)
+                {
+                    levelTexts[i].text = $"Level {i + 1}: {cpuInventory[tier - 1, i].ToString()}";
+                }
+                break;
+            case 2:
+                //gpu
+                for (int i = 0; i < this.levelTexts.Count; i++)
+                {
+                    levelTexts[i].text = $"Level {i + 1}: {gpuInventory[tier - 1, i].ToString()}";
+                }
+                break;
+            case 3:
+                //ram
+                for (int i = 0; i < this.levelTexts.Count; i++)
+                {
+                    levelTexts[i].text = $"Level {i + 1}: {ramInventory[tier - 1, i].ToString()}";
+                }
+                break;
+            case 4:
+                //hdd
+                for (int i = 0; i < this.levelTexts.Count; i++)
+                {
+                    levelTexts[i].text = $"Level {i + 1}: {hddInventory[tier - 1, i].ToString()}";
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    public void SelectComponent(int component)
+    {
+        foreach (var img in componentImages)
+        {
+            img.color = Color.white;
+        }
+        componentImages[component - 1].color = Color.green;
+        this.componentType = component;
+    }
 }
