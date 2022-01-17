7f65f15cc6b25e331d1d9a4f2520162aa82735a7
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2022-01-17 16:33:37 +1100

    move other menus offscreen instead of disable

--- Assets/Scripts/WorkbenchManager.cs
+++ Assets/Scripts/WorkbenchManager.cs
@@ -1,35 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 using UnityEngine.UI;
 using TMPro;
-using System;
 
 public class WorkbenchManager : MonoBehaviour
 {
-    public GameObject tierUpgradeMenu;
-    public GameObject levelUpgradeMenu;
+    public RectTransform tierUpgradeMenu;
+    public RectTransform levelUpgradeMenu;
+    public RectTransform deconstructionMenu;
+    public Vector2 offScreen = 1000 * Vector2.up;
     public TextMeshProUGUI changeMenuButtonText;
+    public List<Image> menuButtonImages;
+    public TextMeshProUGUI scrapText;
 
     private void OnEnable()
     {
-        tierUpgradeMenu.SetActive(true);
-        levelUpgradeMenu.SetActive(false);
+        ChangeMenu(1);
     }
-    public void SwapMenu()
+    public void ChangeMenu(int index)
     {
-        if (tierUpgradeMenu.activeInHierarchy)
+        switch (index)
         {
-            tierUpgradeMenu.SetActive(false);
-            levelUpgradeMenu.SetActive(true);
-            changeMenuButtonText.text = "Combine Components";
+            //Tier Upgrade Menu
+            case 1:
+                tierUpgradeMenu.anchoredPosition = Vector2.zero;
+                levelUpgradeMenu.anchoredPosition = offScreen;
+                deconstructionMenu.anchoredPosition = offScreen;
+                menuButtonImages[0].color = Color.green;
+                menuButtonImages[1].color = Color.white;
+                menuButtonImages[2].color = Color.white;
+                break;
+            //Level Upgrade Menu
+            case 2:
+                tierUpgradeMenu.anchoredPosition = offScreen;
+                levelUpgradeMenu.anchoredPosition = Vector2.zero;
+                deconstructionMenu.anchoredPosition = offScreen;
+                menuButtonImages[0].color = Color.white;
+                menuButtonImages[1].color = Color.green;
+                menuButtonImages[2].color = Color.white;
+                break;
+            //Deconstruction Menu
+            case 3:
+                tierUpgradeMenu.anchoredPosition = offScreen;
+                levelUpgradeMenu.anchoredPosition = offScreen;
+                deconstructionMenu.anchoredPosition = Vector2.zero;
+                menuButtonImages[0].color = Color.white;
+                menuButtonImages[1].color = Color.white;
+                menuButtonImages[2].color = Color.green;
+                break;
+            default:
+                break;
         }
-        else
-        {
-            tierUpgradeMenu.SetActive(true);
-            levelUpgradeMenu.SetActive(false);
-            changeMenuButtonText.text = "Upgrade Component Levels";
-        }
+    }
+    public void UpdateText()
+    {
+        scrapText.text = $"Scrap: {Inventory.Instance.scrap}";
     }
 }
