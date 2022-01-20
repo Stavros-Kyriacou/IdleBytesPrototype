79f97ce9e52892c4b280a9cd949db1eea573b00b
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2022-01-20 15:38:06 +1100

    add RollWeights method

--- Assets/Scripts/Ext.cs
+++ Assets/Scripts/Ext.cs
@@ -27,4 +27,32 @@ public class Ext : MonoBehaviour
         }
         return componentString;
     }
+    public static int RollWeights(List<int> weights)
+    {
+        //Takes an list of item weights and makes a roll on the loot table
+        //Returns the index of the item rolled in the array
+        int total = 0;
+        int index = 0;
+
+        foreach (var weight in weights)
+        {
+            total += weight;
+        }
+
+        int roll = UnityEngine.Random.Range(0, total + 1);
+
+        for (int i = 0; i < weights.Count; i++)
+        {
+            if (roll <= weights[i])
+            {
+                index = i;
+                break;
+            }
+            else
+            {
+                roll -= weights[i];
+            }
+        }
+        return index;
+    }
 }
