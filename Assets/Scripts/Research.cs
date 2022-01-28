aceddd6c4d4e8d6c09f3f8ffa004cbc38e786376
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2022-01-28 17:06:35 +1100

    remove old code

--- Assets/Scripts/Research.cs
+++ Assets/Scripts/Research.cs
@@ -4,106 +4,32 @@ using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
 
-public class Research : MonoBehaviour, IPointerDownHandler
+public class Research : MonoBehaviour
 {
-    public string title;
-    public string description;
-    public int maxLevel;
-    [HideInInspector] public int currentLevel;
-    [SerializeField] private List<Research> researchRequirements;
-    [SerializeField] private Text titleText;
-    public double timeToComplete;
-    public double secondsRemaining;
-    public DateTime timeStarted;
-    public int scrapCost;
-    public bool isUnlocked = false;
-    // public DateTime currentTime;
+    public ResearchManager ResearchManager;
+    public string Title;
+    public string Description;
+    public int MaxLevel;
+    public int CurrentLevel;
+    public List<Research> ResearchRequirements;
+    public int DurationInSeconds;
+    public int ScrapCost;
+    public int GemCost;
+    public ProgressBar ProgressBar;
+    [Header("Text Fields")]
+    public TextMeshProUGUI TitleText;
+    public TextMeshProUGUI DescriptionText;
+    public TextMeshProUGUI LevelText;
     private void Awake()
     {
-        currentLevel = 0;
-        // timeStarted = new DateTime(2021, 10, 16, 13, 33, 0);
-        // currentTime = new DateTime(2021, 10, 5, 17, 20, 0);
-        // var secondsRemaining = (currentTime - timeStarted).TotalSeconds;
-    }
-    private void Start()
-    {
-        titleText.text = title;
-        if (researchRequirements.Count > 0)
-        {
-            description += "\nRequirements:\n";
-            for (int i = 0; i < researchRequirements.Count; i++)
-            {
-                description += researchRequirements[i].title + "\n";
-            }
-        }
-    }
-    public void CalculateTimeRemaining()
-    {
-        if (this.timeStarted == DateTime.MinValue)
-        {
-            Debug.Log("time started is null");
-            this.secondsRemaining = this.timeToComplete;
-        }
-        else
-        {
-            this.secondsRemaining = (WorldTimeAPI.Instance.GetCurrentDateTime() - timeStarted).TotalSeconds;
-        }
-        Debug.Log("Seconds remaining: " + secondsRemaining);
-    }
+        TitleText.text = Title;
+        LevelText.text = $"{this.CurrentLevel}/{this.MaxLevel}";
+        ProgressBar.GetCurrentFill(this.CurrentLevel, this.MaxLevel);
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        CalculateTimeRemaining();
-        ResearchTree.Instance.SelectResearch(this);
     }
-    public void StartResearch()
-    {
-        List<Research> lockedResearch = new List<Research>();
-
-        if (researchRequirements.Count > 0)
-        {
-            foreach (var r in researchRequirements)
-            {
-                if (!r.isUnlocked)
-                {
-                    lockedResearch.Add(r);
-                }
-            }
-        }
-        if (lockedResearch.Count > 0)
-        {
-            Debug.Log("Research cannot be unlocked, requirements not met");
-            foreach (var r in lockedResearch)
-            {
-                Debug.Log(r.title + " still locked");
-            }
-        }
-        else
-        {
-            if (ResearchTree.Instance.scrap >= this.scrapCost && this.currentLevel < this.maxLevel)
-            {
-                ResearchTree.Instance.scrap -= this.scrapCost;
-                this.timeStarted = WorldTimeAPI.Instance.GetCurrentDateTime();
-                Debug.Log("Time started: " + this.timeStarted);
-            }
-            else if (this.currentLevel >= this.maxLevel)
-            {
-                Debug.Log("Research is max level");
-            }
-            else
-            {
-                Debug.Log("Not enough scrap available");
-            }
-        }
-    }
-    public void CompleteResearch()
-    {
-        this.isUnlocked = true;
-        this.currentLevel++;
-        ResearchTree.Instance.UpdateDescriptionBox();
-    }
-
     //a research takes a certain amount of time to complete
     //store the world API time that the research started at
     //when you launch the game, subtract the time started from the current time
@@ -122,7 +48,7 @@ public class Research : MonoBehaviour, IPointerDownHandler
     //save game when you log off
 
     //seconds remaining = 30s
-    
+
     //log in. GetCurrenDateTime from api
     //calculate time since last login (CurrentTime - LastLogOff) in seconds
 
