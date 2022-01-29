07671cd483327b766deb2316de8c6e1adc7f6813
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2022-01-29 18:44:25 +1100

    change StartResearch method. upgrade time remaining, complete research, reset timer

--- Assets/Scripts/ResearchTimer.cs
+++ Assets/Scripts/ResearchTimer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Events;
 using System;
@@ -9,23 +10,48 @@ public class ResearchTimer : MonoBehaviour
 {
     public Timer Timer;
     public ProgressBar ProgressBar;
+    public TextMeshProUGUI TitleText;
     public TextMeshProUGUI TimeRemainingText;
+    public Button CompleteNowButton;
     public Research CurrentResearch;
-    private void Awake()
+    public void StartResearch(Research selectedResearch)
     {
-        Timer.OnTimerCountdown.AddListener(() => ProgressBar.GetCurrentFill(CurrentResearch.DurationInSeconds - Timer.TimeRemaining, CurrentResearch.DurationInSeconds));
+        this.CurrentResearch = selectedResearch;
+        
+        TitleText.text = CurrentResearch.Title;
+
+        TimeSpan time = TimeSpan.FromSeconds(CurrentResearch.DurationInSeconds);
+        TimeRemainingText.text = time.ToString(@"hh\:mm\:ss");
+        ProgressBar.GetCurrentFill(0, CurrentResearch.DurationInSeconds);
+
+        Timer.StartTimer(CurrentResearch.DurationInSeconds);
+        Timer.OnTimerComplete.AddListener(CompleteResearch);
     }
-    public void StartResearch()
+    public void UpdateTimeRemaining()
     {
-        if (CurrentResearch != null)
-        {
-            Timer.StartTimer(CurrentResearch.DurationInSeconds);
-        }
-    }
-    public void UpdateTimeRemainingText()
-    {
+        ProgressBar.GetCurrentFill(CurrentResearch.DurationInSeconds - Timer.TimeRemaining, CurrentResearch.DurationInSeconds);
         TimeSpan time = TimeSpan.FromSeconds(Timer.TimeRemaining);
         TimeRemainingText.text = time.ToString(@"hh\:mm\:ss");
+    }
+    public void CompleteResearch()
+    {
+        //increase the level of current research
+        CurrentResearch.IncreaseLevel();
+        ResetResearchTimer();
+    }
+    public void ResetResearchTimer()
+    {
+        Timer.ResetTimer();
+        TitleText.text = "";
+        TimeRemainingText.text = "00:00:00";
+        ProgressBar.GetCurrentFill(0, 1);
+        CurrentResearch = null;
+        Timer.OnTimerComplete.RemoveAllListeners();
+        //timer.ResetTimer
+        //reset title
+        //reset time remaining text
+        //reset progressbar
+        //current research = null
     }
 
 }
