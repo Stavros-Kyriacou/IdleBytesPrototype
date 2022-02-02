0e97ead80818990c20f04a363d552938d8c56801
Stavros Kyriacou <stavroslkyriacou@gmail.com>
2022-02-02 17:10:55 +1100

    add instant complete method

--- Assets/Scripts/ResearchTimer.cs
+++ Assets/Scripts/ResearchTimer.cs
@@ -8,20 +8,24 @@ using System;
 
 public class ResearchTimer : MonoBehaviour
 {
+    public Research CurrentResearch;
     public Timer Timer;
     public ProgressBar ProgressBar;
+    public Button CompleteNowButton;
+
+    [Header("UI Management")]
     public TextMeshProUGUI TitleText;
     public TextMeshProUGUI TimeRemainingText;
-    public Button CompleteNowButton;
-    public Research CurrentResearch;
+    public TextMeshProUGUI CompleteNowCostText;
     public void StartResearch(Research selectedResearch)
     {
         this.CurrentResearch = selectedResearch;
-        
+
         TitleText.text = CurrentResearch.Title;
 
         TimeSpan time = TimeSpan.FromSeconds(CurrentResearch.DurationInSeconds);
         TimeRemainingText.text = time.ToString(@"hh\:mm\:ss");
+        CompleteNowCostText.text = $"{10 * CurrentResearch.DurationInSeconds} Gems";
         ProgressBar.GetCurrentFill(0, CurrentResearch.DurationInSeconds);
 
         Timer.StartTimer(CurrentResearch.DurationInSeconds);
@@ -32,26 +36,56 @@ public class ResearchTimer : MonoBehaviour
         ProgressBar.GetCurrentFill(CurrentResearch.DurationInSeconds - Timer.TimeRemaining, CurrentResearch.DurationInSeconds);
         TimeSpan time = TimeSpan.FromSeconds(Timer.TimeRemaining);
         TimeRemainingText.text = time.ToString(@"hh\:mm\:ss");
+        CompleteNowCostText.text = $"{10 * this.Timer.TimeRemaining} Gems";
     }
     public void CompleteResearch()
     {
-        //increase the level of current research
-        CurrentResearch.IncreaseLevel();
-        ResetResearchTimer();
+        if (this.CurrentResearch != null)
+        {
+            this.CurrentResearch.IncreaseLevel();
+            ResetResearchTimer();
+        }
+        else
+        {
+            Debug.Log("A research has not been selected");
+        }
+    }
+    public void CompleteResearchInstantly()
+    {
+        if (this.CurrentResearch != null)
+        {
+
+            if (!this.Timer.IsComplete)
+            {
+                if (Inventory.Instance.RemoveGems(10 * this.Timer.TimeRemaining))
+                {
+                    //complete
+                    CompleteResearch();
+                }
+                else
+                {
+                    Debug.Log("Not enough gems");
+                }
+            }
+            else
+            {
+                Debug.Log("Research is already complete");
+            }
+        }
+        else
+        {
+            Debug.Log("A research has not been selected");
+        }
     }
     public void ResetResearchTimer()
     {
-        Timer.ResetTimer();
-        TitleText.text = "";
-        TimeRemainingText.text = "00:00:00";
-        ProgressBar.GetCurrentFill(0, 1);
-        CurrentResearch = null;
-        Timer.OnTimerComplete.RemoveAllListeners();
-        //timer.ResetTimer
-        //reset title
-        //reset time remaining text
-        //reset progressbar
-        //current research = null
+        this.Timer.ResetTimer();
+        this.TitleText.text = "";
+        this.TimeRemainingText.text = "00:00:00";
+        this.CompleteNowCostText.text = "0 Gems";
+        this.ProgressBar.GetCurrentFill(0, 1);
+        this.CurrentResearch = null;
+        this.Timer.OnTimerComplete.RemoveAllListeners();
     }
 
 }
