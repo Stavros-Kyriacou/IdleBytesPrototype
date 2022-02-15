127b4fb9f914967e521b7aefd7c3533f7bf08117
Stavros Kyriacou <stavroslkyriacou@gmail.com>
2022-02-15 14:50:50 +1100

    add new perk methods

--- Assets/Scripts/Perk.cs
+++ Assets/Scripts/Perk.cs
@@ -3,8 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 
 public class Perk : MonoBehaviour
 {
-    
+    [Header("Perk Details")]
+    public string Title;
+    public string Description;
+    public int MaxLevel;
+    public int Cost
+    {
+        get
+        {
+            return (currentLevel + 1) * 5;
+        }
+        private set { }
+    }
+
+    [Header("UI")]
+    public Button PerkButton;
+    public ProgressBar LevelProgressBar;
+    public TextMeshProUGUI TitleText;
+    public TextMeshProUGUI LevelText;
+
+    [Header("References")]
+    public PerkMenu PerkMenu;
+
+    //Local Variables
+    [HideInInspector] public int currentLevel;
+
+    public Perk()
+    {
+        this.currentLevel = 0;
+    }
+    private void OnEnable()
+    {
+        this.PerkButton?.onClick.AddListener(() => PerkMenu.TogglePerkPopup(true));
+        this.PerkButton?.onClick.AddListener(() => PerkMenu.UpdatePerkPopupUI(this));
+    }
+    private void OnDisable()
+    {
+        this.PerkButton.onClick.RemoveAllListeners();
+    }
+    private void Start()
+    {
+        this.TitleText.text = this.Title;
+        this.LevelText.text = $"{this.currentLevel}/{this.MaxLevel}";
+    }
+    public void IncreaseLevel()
+    {
+        if (this.currentLevel < this.MaxLevel)
+        {
+            this.currentLevel++;
+            UpdateLevelProgressBar();
+        }
+    }
+    public void UpdateLevelProgressBar()
+    {
+        this.LevelText.text = $"{this.currentLevel}/{this.MaxLevel}";
+        this.LevelProgressBar.UpdateFill(this.currentLevel, this.MaxLevel);
+    }
 }
