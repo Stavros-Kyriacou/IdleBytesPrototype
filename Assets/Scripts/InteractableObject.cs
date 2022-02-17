37f4117c14d8f94919f6f25c74ebd85a56d29f2a
Stavros Kyriacou <stavroslkyriacou@gmail.com>
2022-02-17 17:06:16 +1100

    comments

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -16,6 +16,7 @@ public class InteractableObject : MonoBehaviour
     {
         if (MenuController.Instance.IsMenuOpen) return;
 
+        //can add some small wiggle room if it doesnt feel responsive enough. E.g if the distance is within 0.1f or something
         if (this.CameraPosition != MainCam.transform.position) return;
 
         this.OnClick.Invoke();
