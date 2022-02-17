d363518f6811d23bf8b38e00b961f1acd53a10a4
Stavros Kyriacou <stavroslkyriacou@gmail.com>
2022-02-17 16:21:49 +1100

    add OnMouseDown and OnMouseUp methods

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InteractableObject : MonoBehaviour
 {
-    
+    public Camera MainCam;
+    public UnityEvent OnClick;
+    private Vector3 CameraPosition;
+    private void OnMouseDown()
+    {
+        this.CameraPosition = this.MainCam.transform.position;
+    }
+    private void OnMouseUp()
+    {
+        if (MenuController.Instance.IsMenuOpen) return;
 
+        if (this.CameraPosition != MainCam.transform.position) return;
+
+        this.OnClick.Invoke();
+    }
 }
